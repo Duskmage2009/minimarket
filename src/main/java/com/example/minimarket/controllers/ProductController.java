504eb794a02241fafd3b9f2dc8f44a3684384d83
Kirill <duskmage2009@gmail.com>
2022-10-28 20:45:50 +0300

    ADD images and links with columns

--- src/main/java/com/example/minimarket/controllers/ProductController.java
+++ src/main/java/com/example/minimarket/controllers/ProductController.java
@@ -31,17 +31,20 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     public String productInfo(@PathVariable Long id, Model model) {
-       /* Product product = productService.getProductById(id);
+        Product product = productService.getProductById(id);
         model.addAttribute("product",product);
-        model.addAttribute("product",product.getImages());*/
-        model.addAttribute("product", productService.getProductById(id));
+        model.addAttribute("images",product.getImages());
+        //model.addAttribute("product", productService.getProductById(id));
         return "product-info";
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product){
+    public String createProduct(@RequestParam("file1")MultipartFile file1,
+                                @RequestParam("file2")MultipartFile file2,
+                                @RequestParam("file3")MultipartFile file3 ,
+                                Product product)throws IOException{
 
-        productService.saveProduct(product);
+        productService.saveProduct(product,file1,file2,file3);
         return "redirect:/";
     }
 
